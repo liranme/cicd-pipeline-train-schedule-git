4546c388fd681361a34051b7a9c2c9c0c1c9a39c
liranme <liranmenashe1@gmail.com>
2021-02-21 18:55:52 +0200

    change has be done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-#some comment
+
 var indexRouter = require('./routes/index');
 var trainsRouter = require('./routes/trains');
 
